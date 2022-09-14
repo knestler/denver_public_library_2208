e160d3ebcafe8957e008c80613747e58f59a4190
Krisetn Nestler <kristen.nestler@gmail.com>
2022-09-14 11:22:25 -0400

    I1 complete book class created and test fixed

---
+++ lib/book.rb
@@ -0,0 +1,8 @@
+class Book
+  attr_reader :author, :title, :publication_year
+  
+  def initialize(attributes)
+    @author = attributes[:author_first_name] + " " + attributes[:author_last_name]
+    @title = attributes[:title]
+    @publication_year = attributes[:publication_date].split(//).last(4).join  end
+end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Book do
   
   it 'exists' do 
     book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
-    expect(book).to be_an_instance_of(Book))
+    expect(book).to be_an_instance_of(Book)
   end
   it 'has attributes' do 
     book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
