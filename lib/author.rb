28538843e6f3aae22f66dcfb9f7cd0055be46589
Krisetn Nestler <kristen.nestler@gmail.com>
2022-09-14 12:02:30 -0400

    I2 complete author class and fixed tests

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,17 @@
 class Author
+  attr_reader :name, :books
   
+  def initialize(attributes)
+    @name = attributes[:first_name] + " " + attributes[:last_name]
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @books = []
+  end
+
+  def write(title, date)
+    book_info = {author_first_name: @first_name, author_last_name: @last_name, title: title, publication_date: date}
+    book = Book.new(book_info)  
+    @books << book
+    return book
+  end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -5,20 +5,21 @@ RSpec.describe Book do
   
   it 'exists' do 
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    expect(charlotte_bronte).to be_an_instance_of(Author)
   end 
 
   it 'has attributes' do 
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
     expect(charlotte_bronte.name).to eq("Charlotte Bronte")
     expect(charlotte_bronte.books).to eq([])
   end 
 
   it 'can take create a new book and display authors books' do 
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
     jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
     expect(jane_eyre.class).to eq(Book)
     expect(jane_eyre.title).to eq("Jane Eyre")
-
     villette = charlotte_bronte.write("Villette", "1853")
-
-    expect(charlotte_bronte.books).to eq(jane_eyre, villette)
+    expect(charlotte_bronte.books).to eq([jane_eyre, villette])
   end
 end
